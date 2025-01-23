96cfb7cdb79b9710669a913832b1a906000872f7
bashchap <taralalram@gmail.com>
2025-01-23 14:46:25 +0700

    Merge here.  Next step is to plot only 'what's in front'.  Need to sort front to back, have a vanishing point/ max rendering depth (beyond which I'll stop drawing).  Need 2d screen 'array' to determine if a there's no need to render something behind it.  ALso need to create an object list and learn how to traverse it

--- mk_grid_data.py
+++ mk_grid_data.py
@@ -3,14 +3,17 @@ from sys import argv
 from math import sqrt
 #import random
 
-MYPOS_X = 192
-MYPOS_Y = 192
-MYPOS_Z = 64
+ORIGIN_X = 0
+ORIGIN_Y = 0
+ORIGIN_Z = 0
 
-GRID_WIDTH = 384
-GRID_HEIGHT =64
-GRID_DEPTH = 1024
+GRID_WIDTH = 128
+GRID_HEIGHT = 64
+GRID_DEPTH = 256
 GRID_BOX = 32
+GRID_BOX_X = 32
+GRID_BOX_Y = 32
+GRID_BOX_Z = 256
 
 #'''These constants determine the resolution of fill of a GRID_BOX'''
 GRID_Z_INC_MIN = 1
@@ -19,9 +22,9 @@ GRID_X_INC_MIN = 1
 
 
 #'''These constants define how quickly the major coordinates are navigated'''
-grid_Z_inc = GRID_BOX
-grid_X_inc = GRID_BOX
-grid_Y_inc = GRID_BOX
+GRID_Z_INC_MAJ = GRID_BOX_Z
+GRID_Y_INC_MAJ = GRID_BOX_Y
+GRID_X_INC_MAJ = GRID_BOX_X
 
 FILENAME3D = 'plot_data_3d.csv'
 PLOTDATA3D_FILE = open(FILENAME3D, 'w')
@@ -42,48 +45,52 @@ def write_grid2d_xyz(x, y):
     data = """\n%3d,%3d,%3d""" % (x, y, 0)
     PLOTDATA2D_FILE.write(data)
 
-def write_grid_box(pg_x, pg_y, pg_z):
-    for z in range(pg_z, pg_z + grid_Z_inc, GRID_Z_INC_MIN):
+def write_grid_box(wgb_x, wgb_y, wgb_z):
+    for z in range(wgb_z, wgb_z + GRID_Z_INC_MAJ, GRID_Z_INC_MIN):
 
-        for y in range (pg_y, pg_y + grid_Y_inc, GRID_Y_INC_MIN):
+        for y in range (wgb_y, wgb_y + GRID_Y_INC_MAJ, GRID_Y_INC_MIN):
 
-            for x in range (pg_x, pg_x + grid_X_inc, GRID_X_INC_MIN):
+            for x in range (wgb_x, wgb_x + GRID_X_INC_MAJ, GRID_X_INC_MIN):
 
 # Determine what pixel to write to form an empty box
 # 1st condition successfully draws front and back faces of box
 # 2nd condition draws top and bottom
 # 3rd condition draws the sizes
-#                if (z == pg_z or (z == pg_z + grid_Z_inc - GRID_Z_INC_MIN)) \
-#                or ((z > pg_z) > 0 and (y == pg_y or y == pg_y + grid_Y_inc - GRID_Y_INC_MIN)) \
-#                or (z > pg_z and (x == pg_x or x == pg_x + grid_X_inc - GRID_X_INC_MIN)):
+#                if (z == wgb_z or (z == wgb_z + GRID_Z_INC_MAJ - GRID_Z_INC_MIN)) \
+#                or ((z > wgb_z) > 0 and (y == wgb_y or y == wgb_y + GRID_Y_INC_MAJ - GRID_Y_INC_MIN)) \
+#                or (z > wgb_z and (x == wgb_x or x == wgb_x + GRID_X_INC_MAJ - GRID_X_INC_MIN)):
 
-                z_edge = (z == pg_z) or (z == pg_z + grid_Z_inc - GRID_Z_INC_MIN)
-                y_edge = (y == pg_y) or (y == pg_y + grid_Y_inc - GRID_Y_INC_MIN)
-                x_edge = (x == pg_x) or (x == pg_x + grid_X_inc - GRID_X_INC_MIN)
+# Will be True if an axis is at it's edge (front or back, top or bottom, left or right
+                z_edge = (z == wgb_z) or (z == wgb_z + GRID_Z_INC_MAJ - GRID_Z_INC_MIN)
+                y_edge = (y == wgb_y) or (y == wgb_y + GRID_Y_INC_MAJ - GRID_Y_INC_MIN)
+                x_edge = (x == wgb_x) or (x == wgb_x + GRID_X_INC_MAJ - GRID_X_INC_MIN)
 
+# Conditions determine if when an edge is visible so can plot an outline
                 if (z_edge and (y_edge or x_edge)) or \
                    ((not z_edge) and (x_edge and y_edge)):
 
-# Adjust coordinates for supplied position
-                    rel_x = x - MYPOS_X
-                    rel_y = y - MYPOS_Y
-                    rel_z = z - MYPOS_Z
+# Create relative coordinates for supplied position
+                    rel_x = x - ORIGIN_X
+                    rel_y = y - ORIGIN_Y
+                    rel_z = z - ORIGIN_Z
+
                     write_grid_xyz(rel_x, rel_y, rel_z)
 
 # Calculate 3d to 2d coordinates and add in perspective
-#                    rel_xy_SQRT=sqrt(rel_x **2 + rel_y **2)
+                    rel_xy_SQR=rel_x **2 + rel_y **2
+                    rel_xy_SQRT=sqrt(rel_xy_SQR)
+                    rel_xyz_SQRT=sqrt(rel_xy_SQR + rel_z **2)
 #                    rel_xyz_SQRT=sqrt(rel_x **2 + rel_y **2 + rel_z **2)
-                    rel_xy_SQRT=sqrt(rel_x **2 + rel_y **2)
-                    rel_xyz_SQRT=sqrt(rel_x **2 + rel_y **2 + rel_z **2)
-#                    rel_xyz_SQRT=(rel_x **2 + rel_y **2 + rel_z **2) ** (1 / 3)
-                    if rel_xyz_SQRT != 0 and rel_z >=0 :
-#                    if rel_xy_SQRT != 0:
+
+                    if rel_xyz_SQRT != 0 and rel_z >= 0 :
                         depthRatio = rel_xy_SQRT / rel_xyz_SQRT
                         per_x = int(rel_x * depthRatio)
                         per_y = int(rel_y * depthRatio)
+
                         print("%4d %4d %4d - %4d %4d - %3.4f %3.4f %3.4f" % \
                         (rel_x, rel_y, rel_z, per_x, per_y, rel_xy_SQRT, \
                         rel_xyz_SQRT, depthRatio))
+
                         write_grid2d_xyz(per_x, per_y)
 
 grid_X = 0
@@ -113,10 +120,10 @@ while grid_Z < GRID_DEPTH:
                 write_grid_box(grid_X, grid_Y, grid_Z)
 
 #'''Move to the next X grid point'''
-            grid_X += grid_X_inc
+            grid_X += GRID_X_INC_MAJ
 #'''Move to the next Y grid point'''
-        grid_Y += grid_Y_inc
+        grid_Y += GRID_Y_INC_MAJ
 #'''Move to the next Z grid point'''
-    grid_Z += grid_Z_inc
+    grid_Z += GRID_Z_INC_MAJ
 
 PLOTDATA3D_FILE.close()
