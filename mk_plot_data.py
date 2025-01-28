f4122166f09dd58fecddfe799e93bb11d1c6a4cb
Tara Ram <taralalram@gmail.com>
2025-01-28 21:23:01 +0700

    I f'ed around and found out. I need to understand why my screen coordinates are insane compare to my mataplot values

--- mk_plot_data.py
+++ mk_plot_data.py
@@ -1,6 +1,8 @@
 # To generate 3d and 2d (transformed) coordinates for use by plot.
 from sys import argv
 from math import sqrt
+import curses
+import time
 #import random
 
 xy_plane = [0]
@@ -9,17 +11,18 @@ xy_total = 0
 xyz_total = 0
 frame = 0
 
-GRID_WIDTH = 32
-GRID_HEIGHT = 32
+GRID_WIDTH = 64
+GRID_HEIGHT = 64
 GRID_DEPTH = 32
-GRID_BOX = 8
+GRID_BOX = 32
 GRID_BOX_X = GRID_BOX
 GRID_BOX_Y = GRID_BOX
 GRID_BOX_Z = GRID_BOX
 
-BASE_ORIGIN_X = int( GRID_WIDTH / 2 - (GRID_BOX_X / 2) )
-BASE_ORIGIN_Y = int( GRID_HEIGHT / 2 - (GRID_BOX_Y / 2) )
-BASE_ORIGIN_Z = int( GRID_DEPTH / 2 - (GRID_BOX_Z / 2) )
+BASE_ORIGIN_X = 0
+BASE_ORIGIN_Y = 0
+#BASE_ORIGIN_Z = int( GRID_DEPTH / 2 - (GRID_BOX_Z / 2) )
+BASE_ORIGIN_Z = 0
 
 #'''These constants determine the resolution of fill of a GRID_BOX'''
 GRID_Z_INC_MIN = 1
@@ -32,10 +35,6 @@ GRID_X_INC_MAJ = GRID_BOX_X
 GRID_Y_INC_MAJ = GRID_BOX_Y
 GRID_Z_INC_MAJ = GRID_BOX_Z
 
-FILENAME4D = 'plot_data_4d.csv'
-PLOTDATA4D_FILE = open(FILENAME4D, 'w')
-PLOTDATA4D_FILE.truncate()
-
 FILENAME3D = 'plot_data_3d.csv'
 PLOTDATA3D_FILE = open(FILENAME3D, 'w')
 PLOTDATA3D_FILE.truncate()
@@ -47,10 +46,6 @@ PLOTDATA2D_FILE.truncate()
 def print_grid_xyz(x, y, z):
     print ("%3d,%3d,%3d" % (x, y, z))
 
-def write_grid4d_fxyz(f, x, y, z):
-    data = """\n%4d,%4d,%4d,%4d""" % (f, x, y, z)
-    PLOTDATA4D_FILE.write(data)
-
 def write_grid3d_xyz(x, y, z):
     data = """\n%3d,%3d,%3d""" % (x, y, z)
     PLOTDATA3D_FILE.write(data)
@@ -61,6 +56,7 @@ def write_grid2d_xyz(x, y):
 
 def write_grid_box(wgb_x, wgb_y, wgb_z):
     global ORIGIN_Z
+#    print(f"ORIGIN_Z: {ORIGIN_Z}")
     global xy_plane, xy_dups, xy_total, xyz_total, frame
     for z in range(wgb_z, wgb_z + GRID_Z_INC_MAJ, GRID_Z_INC_MIN):
         z_edge = (z == wgb_z) or (z == wgb_z + GRID_Z_INC_MAJ - GRID_Z_INC_MIN)
@@ -90,12 +86,10 @@ def write_grid_box(wgb_x, wgb_y, wgb_z):
                     rel_y = y - BASE_ORIGIN_Y
                     rel_z = z - ORIGIN_Z
 
-# Create data for animation (4D frame) in a different file
-                    write_grid4d_fxyz(frame, rel_x, rel_y, rel_z)
-
 # Write 2D & 3D coordinates only for first frame
-                    if frame == 0:
-                        write_grid3d_xyz(rel_x, rel_y, rel_z)
+                    if frame != 999999: # old code, remove it later, don't care about the frame here
+                        if frame == 0:
+                            write_grid3d_xyz(rel_x, rel_y, rel_z)
 
 # Calculate 3d to 2d coordinates and add in perspective
                         rel_xy_SQR=rel_x **2 + rel_y **2
@@ -108,20 +102,46 @@ def write_grid_box(wgb_x, wgb_y, wgb_z):
                             per_x = int(rel_x * depthRatio)
                             per_y = int(rel_y * depthRatio)
 
-                            print("%4d %4d %4d - %4d %4d - %3.4f %3.4f %3.4f" % \
-                            (rel_x, rel_y, rel_z, per_x, per_y, rel_xy_SQRT, \
-                            rel_xyz_SQRT, depthRatio))
+#                            print("%4d %4d %4d - %4d %4d - %3.4f %3.4f %3.4f" % \
+#                            (rel_x, rel_y, rel_z, per_x, per_y, rel_xy_SQRT, \
+#                            rel_xyz_SQRT, depthRatio))
 
-    # If a 2D coordinate has previously been calculated skip it, otherwise store the coordinates.
-    # This is because we've already translated from 3D to 2D so anything else at that coordinate is behind what's
-    # already been drawn (since we're drawing from the direction we're facing forward only).
-                            xy_plane_index = per_x, per_y
+# If a 2D coordinate has previously been calculated skip it, otherwise store the coordinates.
+# This is because we've already translated from 3D to 2D so anything else at that coordinate is behind what's
+# already been drawn (since we're drawing from the direction we're facing forward only).
+# Code here to write to screen
+                            screen_x = int( (per_x + BASE_ORIGIN_X) ) 
+                            screen_y = int( (per_y + BASE_ORIGIN_Y) )
+
+                            xy_plane_index = screen_x, screen_y
                             if xy_plane_index not in xy_plane:
                                 xy_plane.append(xy_plane_index)
-                                write_grid2d_xyz(per_x, per_y)                           
+                                if frame == 0:
+                                    write_grid2d_xyz(screen_x, screen_y)
+
+
+                                if not (screen_x < 1 or screen_y < 1):
+                                    curses.wrapper(plot_char, screen_x, screen_y)
+
+
+#                                print(f"> 3dx = {x:3d}  3dy = {y:3d}  2dx = {per_x:3d}  2dy = {per_y:3d}")
+
+#                                else:
+#                                    print(f"> 3dx = {x:3d}  3dy = {y:3d}  2dx = {per_x:3d}  2dy = {per_y:3d}")
                             else:
                                 xy_dups += 1
 
+def plot_char(stdscr, x, y):
+    stdscr.addch(y, x, "#")
+
+def show_screen(stdscr):    
+    stdscr.refresh()
+
+def clear_screen(stdscr):
+    stdscr.clear()
+
+def screen_wait(stdscr):
+    stdscr.getch()
 
 
 def print_totals():
@@ -130,11 +150,8 @@ def print_totals():
     print("Total 2D points: %d" % xy_total)
     print("Total 2D duplicates: %d" % xy_dups)
 
-
-PLOTDATA4D_FILE.write("Frame,X,Y,Z")
 PLOTDATA3D_FILE.write("X,Y,Z")
 PLOTDATA2D_FILE.write("X,Y,Z")
-write_grid4d_fxyz(0, 0, 0, 0)
 write_grid3d_xyz(0, 0, 0)
 write_grid2d_xyz(0,0)
 
@@ -173,12 +190,17 @@ def walk_the_grid():
 # |_| |_| |_|\__,_|_|_| |_|
 
 FRAME_MAX = int( GRID_BOX_Z * 2 / GRID_Z_INC_MIN )
-for ORIGIN_Z in range(BASE_ORIGIN_Z, BASE_ORIGIN_Z + GRID_BOX_Z * 2, GRID_Z_INC_MIN):
-    print(f"> Progress: Frame {frame} : {100 / FRAME_MAX * frame:3.4f}\%")
+for ORIGIN_Z in range(BASE_ORIGIN_Z, BASE_ORIGIN_Z + GRID_BOX_Z * 10, GRID_X_INC_MIN ):
+#    print(f"> Progress: Frame {frame:3d} : {100 / FRAME_MAX * frame:3.4f} %")
+    curses.wrapper(clear_screen)
     walk_the_grid()
+    curses.wrapper(show_screen)
+    xy_plane.clear()
+    time.sleep(0.5)
     frame += 1
 
+
+#curses.wrapper(screen_wait)
 PLOTDATA2D_FILE.close()
 PLOTDATA3D_FILE.close()
-PLOTDATA4D_FILE.close()
 print_totals()
