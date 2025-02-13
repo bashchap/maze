285413edeb0077bd38189e25469f119ab5f8a398
Tara Ram <taralalram@gmail.com>
2025-02-13 13:02:03 +0700

    Refactored different axis functions into single function as same operations have to occur to different pairs of parameters

--- object.py
+++ object.py
@@ -4,42 +4,7 @@ from math import sqrt
 import math
 import curses
 import time
-#import random
-
-xy_plane = []
-xy_dups = 0
-xy_total = 0
-xyz_total = 0
-current_frame = 0
-
-GRID_RATIO = 4
-
-GRID_WIDTH = 32 * GRID_RATIO
-GRID_HEIGHT =32 * GRID_RATIO
-GRID_DEPTH = 32 * GRID_RATIO
-
-GRID_BOX = 32 * GRID_RATIO
-
-GRID_BOX_X = 16 * GRID_RATIO
-GRID_BOX_Y = 16 * GRID_RATIO
-GRID_BOX_Z = 16 * GRID_RATIO
-
-BASE_ORIGIN_X = GRID_WIDTH / 2
-BASE_ORIGIN_Y = GRID_HEIGHT / 2
-BASE_ORIGIN_Z = GRID_DEPTH / 2
-
-#'''These constants determine the resolution of fill of a GRID_BOX'''
-GRID_Z_INC_MIN = 2
-GRID_Y_INC_MIN = 2
-GRID_X_INC_MIN = 2
-
-
-#'''These constants define how quickly the major coordinates are navigated'''
-GRID_X_INC_MAJ = GRID_BOX_X
-GRID_Y_INC_MAJ = GRID_BOX_Y
-GRID_Z_INC_MAJ = GRID_BOX_Z
-
-
+import random
 
 def print_grid_xyz(x, y, z):
     print ("%3d,%3d,%3d" % (x, y, z))
@@ -84,46 +49,16 @@ def write_grid_box(wgb_x, wgb_y, wgb_z):
                     rel_y = y - ORIGIN_Y
                     rel_z = z - ORIGIN_Z
 
-# Perform rotation around Z axis - THIS WORKS!!!
-                    xp, yp, zp = rel_x, rel_y, rel_z
-#                    print(f"b4 rotation: xp = {xp}, yp = {yp}, zp = {zp}")                    
-                    z_rotation_val = math.radians(z_rotation)
-#                    Zxp = xp * math.sin(z_rotation_val) - yp * math.cos(z_rotation_val)
-#                    Zyp = xp * math.cos(z_rotation_val) + yp * math.sin(z_rotation_val)
-                    Zxp = xp * math.cos(z_rotation_val) - yp * math.sin(z_rotation_val)
-                    Zyp = xp * math.sin(z_rotation_val) + yp * math.cos(z_rotation_val)                    
-                    xp, yp = Zxp, Zyp
-#                    print(f"Z rotation: xp = {xp}, yp = {yp}, zp = {zp}")
+                    xp, yp = XYZ_ROTATION(z_rotation, rel_x, rel_y)
+                    zp, yp = XYZ_ROTATION(x_rotation, rel_z, yp)
+                    xp, zp = XYZ_ROTATION(y_rotation, xp, zp)
 
-# Perform rotation around X axis
-                    x_rotation_val = math.radians(x_rotation)
-#                    Xzp = zp * math.sin(x_rotation_val) - yp * math.cos(x_rotation_val)
-#                    Xyp = zp * math.cos(x_rotation_val) + yp * math.sin(x_rotation_val)
-                    Xzp = zp * math.cos(x_rotation_val) - yp * math.sin(x_rotation_val)
-                    Xyp = zp * math.sin(x_rotation_val) + yp * math.cos(x_rotation_val)
+                    zp += GRID_BOX_Z * 2
 
-                    yp, zp = Xyp, Xzp
-#                    print(f"X rotation: xp = {xp}, yp = {yp}, zp = {zp}")
-
-# Perform rotation around Y axis
-                    y_rotation_val = math.radians(y_rotation)
-#                    Yxp = xp * math.sin(y_rotation_val) - zp * math.cos(y_rotation_val)
-#                    Yzp = xp * math.cos(y_rotation_val) + zp * math.sin(y_rotation_val)
-                    Yxp = xp * math.cos(y_rotation_val) - zp * math.sin(y_rotation_val)
-                    Yzp = xp * math.sin(y_rotation_val) + zp * math.cos(y_rotation_val)                    
-                    xp, zp = Yxp, Yzp
-#                    print(f"Y rotation: xp = {xp}, yp = {yp}, zp = {zp}")
-#                    quit()
 
 # Write 2D & 3D coordinates only for first frame
                     if current_frame != -1 :
                         write_grid3d_xyz(xp, yp, zp)
-
-# Calculate 3d to 2d coordinates and add in perspective
-#                    rel_xy_SQR = rel_x **2 + rel_y **2
-#                    rel_xy_SQRT = sqrt(rel_xy_SQR)
-#                    rel_xyz_SQR = rel_xy_SQR + rel_z **2
-#                    rel_xyz_SQRT = sqrt(rel_xyz_SQR)
 
                     if zp != -999:
                         xy_total += 1
@@ -141,14 +76,6 @@ def write_grid_box(wgb_x, wgb_y, wgb_z):
                         else:
                             xy_dups += 1
 
-def print_debug(frame, x, y, z, rel_x, rel_y, rel_z, rel_xy_SQR, rel_xy_SQRT, rel_xyz_SQR, rel_xyz_SQRT, depthRatio, per_x, per_y, screen_x, screen_y):
-    print(f"{frame},{x},{y},{z},{rel_x},{rel_y},{rel_z},{rel_xy_SQR},{rel_xy_SQRT},{rel_xyz_SQR},{rel_xyz_SQRT},{depthRatio},{per_x},{per_y},{screen_x},{screen_y}")
-
-def print_debug_header():
-    print(f"frame, x, y, z, rel_x, rel_y, rel_z, rel_xy_SQR, rel_xy_SQRT, rel_xyz_SQR, rel_xyz_SQRT, depthRatio_x, per_x, per_y, screen_x, screen_y")
-
-def plot_char(stdscr, x, y):
-    stdscr.addch(y, x, "#")
 
 def show_screen(stdscr):
     stdscr.refresh()
@@ -170,12 +97,7 @@ def draw_screen():
     x_values , y_values = zip(*xy_plane) # Create x and y lists from xy_plane
  #   x_values = [ x + BASE_ORIGIN_X for x in x_values ] # Add back in the origin
  #   y_values = [ y + BASE_ORIGIN_Y for y in y_values ] # Add back in the origin
-
     curses.wrapper(plot_coordinates)
-
-def normalize(value, min_val, max_val, target_min, target_max):
-    """Normalize values to fit within target range."""
-    return int((value - min_val) / (max_val - min_val) * (target_max - target_min) + target_min)
 
 def plot_coordinates(stdscr):
     curses.curs_set(0)  # Hide the cursor
@@ -184,27 +106,15 @@ def plot_coordinates(stdscr):
     screen_center_x = width / 2
     screen_centre_y = height / 2
     
-    # Get min and max for normalization
-    # min_x, max_x = min(x_values), max(x_values)
-    # min_y, max_y = min(y_values), max(y_values)
-    
     min_x, min_y = 1, 1
-    max_x, max_y = width - 1, height - 1
-    
-    # Avoid division by zero
-    #if min_x == max_x:
-    #    max_x += 1
-    #if min_y == max_y:
-    #    max_y += 1
+    max_x, max_y = width, height
     
     for x, y in zip(x_values, y_values):
-#        screen_x = normalize(x, min_x, max_x, 1, width - 2)
-#        screen_y = normalize(y, min_y, max_y, 1, height - 2)
         screen_x = int( screen_center_x + x )
         screen_y = int( screen_centre_y + y )
-        if not (screen_x < 0 or screen_x >= width - 1 or screen_y < 0 or screen_y >= height - 1):
+        if not (screen_x < 0 or screen_x > width - 1 or screen_y < 0 or screen_y > height - 1):
             try:
-                stdscr.addch(screen_y, screen_x, '*')
+                stdscr.addch(screen_y, screen_x, chr(0x2588))                
             except curses.error:
                 pass  # Ignore errors when trying to plot outside boundaries
 
@@ -244,37 +154,86 @@ def walk_the_grid():
     #'''Move to the next Z grid point'''
         grid_Z += GRID_Z_INC_MAJ
 
+def XYZ_ROTATION(theta, p1, p2):
+    # For rotation along the Z, X or Y axis, supply the required values for p1 & p2 and
+    #  the returned values are for those axis, i.e.:
+    #                                                   Z = xp  yp,  X = zp  yp, Y = xp zp
+    theta_rads = math.radians(theta)
+    p1_prime = p1 * math.cos(theta_rads) - p2 * math.sin(theta_rads)
+    p2_prime = p1 * math.sin(theta_rads) + p2 * math.cos(theta_rads)                    
+    return p1_prime, p2_prime
+
+#   ___ _   _ ___ _____ 
+#   |_ _| \ | |_ _|_   _|
+#    | ||  \| || |  | |  
+#    | || |\  || |  | |  
+#   |___|_| \_|___| |_|  
+#                     
+
+
+GRID_RATIO = 6
+
+GRID_WIDTH = 64 * GRID_RATIO
+GRID_HEIGHT =32 * GRID_RATIO
+GRID_DEPTH = 16 * GRID_RATIO
+
+GRID_BOX = 32 * GRID_RATIO
+
+GRID_BOX_X =  16 * GRID_RATIO
+GRID_BOX_Y =  16 * GRID_RATIO
+GRID_BOX_Z =  16 * GRID_RATIO
+
+BASE_ORIGIN_X = GRID_WIDTH / 2
+BASE_ORIGIN_Y = GRID_HEIGHT / 2
+BASE_ORIGIN_Z = GRID_DEPTH / 2
+
+#'''These constants determine the resolution of fill of a GRID_BOX'''
+GRID_Z_INC_MIN = 4
+GRID_Y_INC_MIN = 4
+GRID_X_INC_MIN = 4
+
+#'''These constants define how quickly the major coordinates are navigated'''
+GRID_X_INC_MAJ = GRID_BOX_X
+GRID_Y_INC_MAJ = GRID_BOX_Y
+GRID_Z_INC_MAJ = GRID_BOX_Z
+
+ORIGIN_X = GRID_WIDTH / 2
+ORIGIN_Y = GRID_HEIGHT / 2
+ORIGIN_Z = GRID_DEPTH / 2
+
+
+x_rotation = 0
+x_rotation_inc = int(random.random()*3)
+y_rotation = 0
+y_rotation_inc = int(random.random()*4)
+z_rotation = 0
+z_rotation_inc = int(random.random()*5)
+
 #                  _       
 #  _ __ ___   __ _(_)_ __  
 # | '_ ` _ \ / _` | | '_ \ 
 # | | | | | | (_| | | | | |
 # |_| |_| |_|\__,_|_|_| |_|
 
-
-
-MAX_FRAMES = 256
+xy_plane = []
+xy_dups = 0
+xy_total = 0
+xyz_total = 0
 current_frame = 0
-ORIGIN_X = GRID_WIDTH / 2
-ORIGIN_Y = GRID_HEIGHT / 2
-ORIGIN_Z = GRID_DEPTH / 2
 
-x_rotation = 0
-x_rotation_inc = .5
-y_rotation = 0
-y_rotation_inc = -1 * 2
-z_rotation = 0
-z_rotation_inc = 0
+MAX_FRAMES, current_frame = 512, 0
 
+PLOT_DATA_DIR = "./plotdata"
 while current_frame < MAX_FRAMES:
     file_frame = f"{current_frame:04d}"
 
 #    print(f"Current frame: {current_frame}   Z: {ORIGIN_Z}")
 
-    FILENAME3D = f"plot_data_3d_{file_frame}.csv"
+    FILENAME3D = f"{PLOT_DATA_DIR}/plot_data_3d_{file_frame}.csv"
     PLOTDATA3D_FILE = open(FILENAME3D, 'w')
     PLOTDATA3D_FILE.truncate()
 
-    FILENAME2D = f"plot_data_2d_{file_frame}.csv"
+    FILENAME2D = f"{PLOT_DATA_DIR}/plot_data_2d_{file_frame}.csv"
     PLOTDATA2D_FILE = open(FILENAME2D, 'w')
     PLOTDATA2D_FILE.truncate()
 
